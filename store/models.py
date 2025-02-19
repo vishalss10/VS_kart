c0b6c0173b327fb0297b17e0fcd78868a7e0eacb
vishal <suryawanshivishal1710@gmail.com>
2025-02-19 13:56:07 +0530

    add the product detail page and active link to the page

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Products(models.Model):
   product_name = models.CharField(max_length=200 , unique=True)
@@ -12,6 +12,10 @@ class Products(models.Model):
   category = models.ForeignKey('category.Category', on_delete=models.CASCADE)
   created_date = models.DateTimeField(auto_now_add=True)
   modified_date = models.DateTimeField(auto_now=True)
+
+
+  def get_url(self):
+    return reverse('product_detail',args=[self.category.slug,self.slug])
 
   def __str__(self):
     return self.product_name
