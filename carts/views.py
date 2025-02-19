ef7e1043acbaff6dd220835e5bb7c36f239c28ec
vishal <suryawanshivishal1710@gmail.com>
2025-02-20 01:10:50 +0530

    add the cart count in cart icon

---
+++ carts/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Cart,CartItem
+from store.models import Products
+
+# Create your views here.
+def _Cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request,product_id):
+    product = Products.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id= _Cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _Cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            cart = cart,
+            quantity = 1
+        )
+        cart_item.save()
+    return redirect('cart')
+    
+def remove_cart(request,product_id):
+    cart = Cart.objects.get(cart_id= _Cart_id(request))
+    product = get_object_or_404(Products,id=product_id)
+    cart_item = CartItem.objects.get(product=product,cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id= _Cart_id(request))
+    product = get_object_or_404(Products,id=product_id)
+    cart_item = CartItem.objects.get(product=product,cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+def cart(request,total=0,quantity=0,cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id= _Cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = tax + total
+    except Cart.DoesNotExist:
+        pass
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total
+    }
+    return render(request,'store/cart.html',context)
+
